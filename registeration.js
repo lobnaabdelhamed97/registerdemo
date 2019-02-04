ffd2b9a52bd0275759e20127e168f41eccc1abc8
lobnaabdelhamed97 <38727109+lobnaabdelhamed97@users.noreply.github.com>
2019-02-04 10:07:42 +0200

    Update registeration.js

--- registeration.js
+++ registeration.js
@@ -6,16 +6,19 @@ $(document).ready(function() {
     var password = $("#password").val();
     var cpassword = $("#cpassword").val();
     if (name == '' || email == '' || password == '' || cpassword == '') {
-    alert("Please fill all fields");
-    } else if ((password.length) < 8) {
-    alert("Password should atleast 8 character in length");
-    } else if (!(password).match(cpassword)) {
-    alert("Your passwords don't match. Try again?");
-    } 
+    alert("Please fill all fields");}
     else if ((email.indexOf("@", 0) < 0) || (email.indexOf(".", 0) < 0) )
     {
         alert ("please enter a valid email");
     }
+ else if ((password.length) < 8) 
+    {
+    alert("Password should atleast 8 character in length");
+    } 
+ else if (password != cpassword) {
+    alert("Your passwords don't match. Try again?");
+    } 
+   
         else {
 
 
@@ -29,16 +32,9 @@ map[name] = password;
         console.log('name: ' + i + ' password: ' + map[i]);
     }
 }
-//var fs = require('fs');  //undefined require!!!! how to solve this problem????
-//fs.appendFile('registered.txt', name+'\t'+password+'\n', function (err) {
-  //  if (err) throw err;
-  //  console.log('The "data is appended in the filesuccesfully!');
- // });
- // fs.readFile('registered.txt', function (err, data) {
- //   if (err) throw err;
- //   console.log(data);
- // });
- 
-$("form")[0].reset();  }
+
+ window.open("file:///C:/Users/Mohammad/Documents/visualcode%20workspace/registerlogin/welcome.html");
+$("form")[0].reset(); 
+ }
     });
     });
